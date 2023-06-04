068d7e8405f691aeff4fa2f1069062dbb0b75aab
Jacques-Drumond <jacques.junio@hotmail.com>
2023-06-04 13:57:24 -0300

    rendering data

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,10 +5,11 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=50, unique=True)
     page_number = models.IntegerField()
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.title} {self.page_number} pages {self.rating} rating"
+        return f"{self.title} {self.page_number} from {self.author}, pages and {self.rating} rating"
     
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+from django.http import Http404
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+def book_detail(request, id):
+    actual_book = get_object_or_404(Book, pk=id)
+    return render(request,  "book_outlet/book_detail.html", {
+    "title": actual_book.title,
+    "author": actual_book.author,
+    "rating": actual_book.rating,
+    "is_best_selling": actual_book.is_best_selling
+    })
