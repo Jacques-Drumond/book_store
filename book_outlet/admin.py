23c74fbedde03496fd593e960cbf05c7498cf20c
Jacques Drumond <jacques.drumond@matrix.local>
2023-06-07 16:51:41 -0300

    adding authors

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Book
 # Register your models here.
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+
+admin.site.register(Book, BookAdmin)
