500af9d77213880338448158319e1b0478b57219
Jacques Drumond <jacques.drumond@matrix.local>
2023-06-07 17:39:56 -0300

    a

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
@@ -8,4 +11,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author")
 
 
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,8 +5,15 @@ from django.utils.text import slugify
 # Create your models here.
 
 class Author(models.Model):
+    
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
 
 
 class Book(models.Model):
@@ -15,7 +22,7 @@ class Book(models.Model):
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
-    is_best_selling = models.BooleanField(default=False)
+    is_best_selling = models.BooleanField(default=False)    
 
     slug = models.SlugField(default="", null=False, db_index=True)
 
